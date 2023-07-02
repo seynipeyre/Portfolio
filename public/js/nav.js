27fff1b35f4aa22b5579d7c0fb923919fe8e36ec
seynipeyre <seynipeyre@gmail.com>
2023-07-02 14:08:23 +0200

    Portfolio

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,8 +1,27 @@
 function toggleSubMenu(event) {
-    event.preventDefault();
-    const subMenu = event.target.nextElementSibling;
-    subMenu.classList.toggle("active");
-    const hiddenLink = event.target.parentNode.querySelector(".menus__links__hidden-link");
-    hiddenLink.classList.toggle("hidden");
-  }
+  event.preventDefault();
+  const subMenu = event.target.nextElementSibling;
+  subMenu.classList.toggle("active");
+  event.target.classList.toggle("hidden-link");
+}
+
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   const projetLink = document.getElementById("projet-link");
+//   projetLink.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     toggleSubMenu("projet-link");
+//   });
+// });
+
+
+
+$(document).ready(function() {
+  $('.navbar-toggler').click(function() {
+    $('.navbar-collapse').toggleClass('show');
+  });
+});
+  
+  
   
