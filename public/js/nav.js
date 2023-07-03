f5672489517497c326c720f597cdafd885d4929b
seynipeyre <seynipeyre@gmail.com>
2023-07-03 20:38:23 +0200

    responsive

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,9 +1,57 @@
+// function toggleSubMenu(event) {
+//   event.preventDefault();
+//   const subMenu = event.target.nextElementSibling;
+//   subMenu.classList.toggle("active");
+//   event.target.classList.toggle("hidden-link");
+// }
+
+
+// function toggleSubMenu(event) {
+//   event.preventDefault();
+//   const subMenu = event.target.nextElementSibling;
+
+//   // Vérifiez si le sous-menu est déjà visible
+//   const isSubMenuVisible = subMenu.classList.contains("active");
+
+//   // Fermez tous les sous-menus ouverts
+//   const allSubMenus = document.querySelectorAll(".submenu");
+//   allSubMenus.forEach((menu) => {
+//     menu.classList.remove("active");
+//   });
+
+//   // Si le sous-menu n'est pas visible, ouvrez-le
+//   if (!isSubMenuVisible) {
+//     subMenu.classList.add("active");
+//   }
+// }
+
+// // Utilisez l'événement "touchstart" au lieu de "click"
+// const subMenuLink = document.querySelector(".menus__links__link-wrapper__cta1");
+// subMenuLink.addEventListener("touchstart", toggleSubMenu);
+
+
 function toggleSubMenu(event) {
   event.preventDefault();
   const subMenu = event.target.nextElementSibling;
-  subMenu.classList.toggle("active");
-  event.target.classList.toggle("hidden-link");
+
+  // Vérifiez si le sous-menu est déjà visible
+  const isSubMenuVisible = subMenu.classList.contains("active");
+
+  // Fermez tous les sous-menus ouverts
+  const allSubMenus = document.querySelectorAll(".submenu");
+  allSubMenus.forEach((menu) => {
+    menu.classList.remove("active");
+  });
+
+  // Si le sous-menu n'est pas visible, ouvrez-le
+  if (!isSubMenuVisible) {
+    subMenu.classList.add("active");
+  }
 }
+
+// Utilisez l'événement "click"
+const subMenuLink = document.querySelector(".menus__links__link-wrapper__cta1");
+subMenuLink.addEventListener("click", toggleSubMenu);
 
 
 
